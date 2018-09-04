84ff19907ffa56e9fc38fe087b43593edaf3bbb3
Michał Lipiński <cybjaz@michal-lipinski-1.laptop.matinf.uj.edu.pl>
2018-09-04 10:18:33 +0200

    Parallel script for computing wasserstein kernel with python

--- wasserstein_py/pd_wasserstein.py
+++ wasserstein_py/pd_wasserstein.py
@@ -1,5 +1,6 @@
 import dionysus as d
 import numpy as np
+import multiprocessing as mp
 
 def wasserstein_dist_mat(pds):
     diag = pds2diagrams(pds);
@@ -7,7 +8,8 @@ def wasserstein_dist_mat(pds):
     K = np.zeros((n, n));
     for i in range(n):
         print str(i) + '/' + str(n)
-        for j in range(n):
+        for j in range(i, n):
+            # print str(i) + '/' + str(n) + ' (' + str(j) + ')'
             K[i, j] = d.wasserstein_distance(diag[i], diag[j]);
             K[j, i] = K[i, j];
     return K;
@@ -15,8 +17,54 @@ def wasserstein_dist_mat(pds):
 def pds2diagrams(pds):
     print len(pds)
     diagrams = [];
-#    print range(0, len(pds))
     for i in range(0, len(pds)):
         #diagrams.append(i)
         diagrams.append(d.Diagram([tuple(x) for x in pds[i]]))
     return diagrams
+
+def comp_row(i, p, dic, out):
+    n = dic['n'];
+    diag = dic['diag'];
+    z = n/p + 1;
+
+    I = [x*p+i for x in range(z) if x*p+i < n];
+    
+    for r in I:
+        row = np.zeros(n);
+        lb = r*n;
+        rb = (r+1)*n;
+
+        print str(r) + '/' + str(n)
+        for j in range(r, n):
+            row[j] = d.wasserstein_distance(diag[r], diag[j]);
+        out[lb:rb] = row;
+        # print(row);
+
+def wasserstein_dist_mat_parallel(pds):
+    diag = pds2diagrams(pds);
+    n = len(pds);
+    K = np.zeros((n, n));
+
+    mng = mp.Manager();
+    dic = mng.dict();
+    dic['diag'] = diag;
+    dic['n'] = n;
+    out_arr = mp.Array('f', n*n, lock=False);
+    
+    # p = mp.Pool(4);
+    pool = 4;
+    processes = [];
+    for i in range(pool):
+        p = mp.Process(target=comp_row, args=(i, pool, dic, out_arr));
+        processes.append(p)
+    [x.start() for x in processes]
+
+    for proc in processes:
+        proc.join()
+    K = np.array(out_arr);
+    K = K.reshape((n, n));
+    for i in range(n):
+        for j in range(n):
+            K[j, i] = K[i,j];
+
+    return K;
--- wasserstein_py/test.py
+++ wasserstein_py/test.py
@@ -1,9 +1,20 @@
 import scipy.io as sio
-from pd_wasserstein import wasserstein_dist_mat 
+from pd_wasserstein import wasserstein_dist_mat, wasserstein_dist_mat_parallel
 
-data = sio.loadmat('/home/cybjaz/workspace/pcodebooks/pcodebooks/exp01/pd.mat');
+# path = '/home/cybjaz/workspace/pcodebooks/pcodebooks/rawdata/exp03_geomat/';
+# filename = 'pds_1_400';
+path = '/home/cybjaz/workspace/pcodebooks/pcodebooks/exp01_svm/';
+filename = 'pd';
+
+# data = sio.loadmat('/home/cybjaz/workspace/pcodebooks/pcodebooks/exp01/pd.mat');
+data = sio.loadmat(path + filename + '.mat');
 
 pds = data['pds'];
 
-K = wasserstein_dist_mat(pds.transpose().flatten())
+pds = pds.transpose().flatten();
+K = wasserstein_dist_mat_parallel(pds);
+# K = wasserstein_dist_mat_parallel(pds[0:20]);
 
+print(K);
+
+sio.savemat(path + filename + '_pw.mat', {'K': K });
