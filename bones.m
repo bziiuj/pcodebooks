8dc61d91795e2213b21a4137bfa0bd286a5dfd97
Bartosz Zielinski <bartosz.zielinski@uj.edu.pl>
2020-03-05 12:59:44 +0100

    bones correlation plus bagger regression

--- bones.m
+++ bones.m
@@ -1,3 +1,18 @@
+clear;
+clc;
+
+%%
+
+method_name = 'PFV'; % empty or PI, PBoW, PFV
+k = 100;
+normalized = 0; % 0 or 1
+dimension = 1;
+
+analyze_correlation = 0;
+regression = 1;
+
+%%
+
 addpath('pcontrollers');
 addpath('../pdsphere/matlab/libsvm-3.21/matlab');
 addpath('../pdsphere/matlab');
@@ -12,9 +27,14 @@ for i=1:length(T.subject)
     subject = T.subject{i};
     if sum(strcmp(M.name, subject)) > 0
         pdFile = M.pdFile{strcmp(M.name, subject)};
-        pd = readtable(['/media/data2/data/bones/normalized/', pdFile]);
-        pd = pd(:, 3:4); % possibly limit to those with specific values at the other columns
-        pd = table2array(pd);
+        if normalized
+            pd = readtable(['/media/data2/data/bones/normalized/', pdFile]);
+        else
+            pd = readtable(['/media/data2/data/bones/not_normalized/', pdFile]);
+        end
+        pd = table2array(pd(:, 1:4));
+        pd = pd(pd(:, 2) == dimension, :);
+        pd = pd(:, 3:4);
         pds{i, 1} = pd;
     else
         unknown = [unknown, i];
@@ -27,22 +47,19 @@ limits = [min(min(cell2mat(pds(:)))), max(max(cell2mat(pds(:))))];
 
 %%
 
-method_name = ''; % empty or PI
-
-%%
-
 % Create PersistenceBow object
 if strcmp(method_name, 'PI')
-    method = PersistenceImage(20, 0.1,  @linear_ramp);
-else
-    %method = PersistenceBow(100, @linear_ramp);
-    method = PersistenceFV(100, @linear_ramp);
+    method = PersistenceImage(sqrt(k), 0.1,  @linear_ramp);
+elseif strcmp(method_name, 'PBoW')
+    method = PersistenceBow(k, @linear_ramp);
+elseif strcmp(method_name, 'PFV')
+    method = PersistenceFV(k, @linear_ramp);
 end
 
 % Get PBOWs representation for all examples
 if strcmp(method_name, 'PI')
     reprCell = method.predict(pds(:), limits);
-else
+elseif strcmp(method_name, 'PBoW') || strcmp(method_name, 'PFV')
     % Train PBOW
     method = method.fit(pds, limits);
 
@@ -55,21 +72,36 @@ for i = 1:size(features, 2)
 	features(:, i) = reprCell{i}(:)';
 end
 
-rs = [];
-ps = [];
-for i = 1:size(features, 2)
-    [r, p] = corrcoef(features(i, :)', labels);
-    rs = [rs, r(1, 2)];
-    ps = [ps, p(1, 2)];
-end
-rs = abs(rs);
-[rs_sorted, rs_order] = sort(rs, 'descend');
+if analyze_correlation
+    rs = [];
+    ps = [];
+    for i = 1:size(features, 1)
+        [r, p] = corrcoef(features(i, :)', labels);
+        rs = [rs, r(1, 2)];
+        ps = [ps, p(1, 2)];
+    end
+    rs = abs(rs);
+    [rs_sorted, rs_order] = sort(rs, 'descend');
 
-%Y = tsne(features');
-%scatter(Y(:, 1), Y(:, 2), 25, labels, 'filled');
-figure;
-for i = 1:12
-    subplot(3, 4, i);
-    scatter(features(rs_order(i), :), labels, 5);
-    title([num2str(rs_order(i)), ' = ', num2str(rs_sorted(i))]);
+    %Y = tsne(features');
+    %scatter(Y(:, 1), Y(:, 2), 25, labels, 'filled');
+    figure;
+    for i = 1:12
+        subplot(3, 4, i);
+        scatter(features(rs_order(i), :), labels, 5);
+        title([num2str(rs_order(i)), ' = ', num2str(rs_sorted(i))]);
+    end
+end
+
+if regression
+    rng(1); % For reproducibility
+    Mdl = TreeBagger(100, features', labels, 'Method', 'regression', 'OOBPrediction', 'on');
+    
+    YP = predict(Mdl, features');
+    YoobP = oobPredict(Mdl);
+
+    subplot(1, 2, 1);
+    plot(labels, YP, 'r*')
+    subplot(1, 2, 2);
+    plot(labels, YoobP, 'r*')
 end
